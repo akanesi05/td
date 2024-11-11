4cd3aeb99a5c5b9e36f3e2b6e67bd9cfee4765bc
mitsuiakane <akane.sumitomo@gmail.com>
2024-11-11 16:17:00 +0900

    タスクが削除されるようにする

--- src/App.js
+++ src/App.js
@@ -18,6 +18,14 @@ export const Todo = () => {
     setIncompleteTodos(newTodos);
     setTodoText("");/*入力した後からの配列にする*/
   };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);/*特定の一覧のものを削除する*/
+    setIncompleteTodos(newTodos);/*その値をステートで更新する*/
+  };
+    
+  
   return (
     <>
       <div>
@@ -32,12 +40,12 @@ export const Todo = () => {
         <p className="title">未完了のTODO</p>
         {/*一覧を表示させる */}
         <ul>
-          {incompleteTodos.map((todo) => (
+          {incompleteTodos.map((todo,index) => (
             <li key={todo}>
               <div className="incomplete-area">
                 <p className="title">{todo}</p>
                 <button>完了</button>
-                <button>削除</button>
+                <button  onClick={() => onClickDelete(index)}>削除</button>
               </div>
             </li>
           ))}
